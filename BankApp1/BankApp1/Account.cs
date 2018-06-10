641f488ba97ef581c7eb23734eb152faa97ce151
Devsmita <devsmita.jagtap@gmail.com>
2018-06-10 12:03:42 -0700

    Account class

---
+++ BankApp1/BankApp1/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp1
+{
+    class Account
+    {
+        #region
+        /// <summary>
+        /// Unique account number for the account
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        /// <summary>
+        /// Email address of 
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// Account Current Balance
+        /// </summary>
+        public decimal Balance { get; private set; }
+        /// <summary>
+        /// Account type
+        /// </summary>
+        public string TypeOfAccount { get; set; }
+        /// <summary>
+        /// Create date of the Account
+        /// </summary>
+        public DateTime dateTime { get; set; }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Deposit Amount
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        /// <returns>updated balance</returns>
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        /// <summary>
+        /// Withdraw amount
+        /// </summary>
+        /// <param name="amount">amount to withdraw</param>
+        /// <returns>updated balance</returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+
+    }
+}
---
+++ BankApp1/BankApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            //account.AccountNumber=12345;
+            account.EmailAddress = "test@tesct.com";
+            account.TypeOfAccount = "Checking";
+            //account.Balance = 10000;
+
+            Console.WriteLine(account.Balance);
+            var account2 = new Account();
+            account2.EmailAddress = "test2.test.com";
+        }
+    }
+}
